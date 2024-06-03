5e4b7558a248d11d3a080ed4d54a31220058d959
Artur Serkeli <serkeli2006@gmail.com>
2024-06-03 18:15:35 +0300

    fixed

--- obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e60a489bcca1daafc055edb633950cb4c00594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03865580807153460016bda57812d0eda109ffc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
